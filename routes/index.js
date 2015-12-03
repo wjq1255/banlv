d53d913bd8869cbee3f4438fabe9b3e20c1d570e
wujq <wujq@lianchuang.com>
2015-12-03 18:38:24 +0800

    #1 首页 静态页面

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var userdata = require('../data/userjson');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', {userlist: userdata.userlist});
+  res.json({userlist: userdata.userlist});
+});
+
+router.get('/zhengwen', function(req, res, next) {
+	
+  res.render('zhengwen', {});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,22 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: '登陆' });
+	res.render('login', { title: '登陆' });
+});
+
+router.post('/login', function(req, res, next) {
+	var username = req.body.userName;
+	var pwd = req.body.password;
+	console.log(username+"---"+pwd);
+	res.render('login', { title: username+'登陆' });
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: '注册' });
+	res.render('register', { title: '注册' });
 });
 
 router.get('/forget', function(req, res, next) {
-  res.render('forget', { title: '忘记密码' });
+	res.render('forget', { title: '忘记密码' });
 });
 
 module.exports = router;
